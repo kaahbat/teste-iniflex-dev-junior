25528c65e168746f0146f4d106a8e34a000f99d3
kaahbat <carlos.hns@aluno.ufop.edu.br>
2025-09-04 20:43:33 -0300

    refactor: Mover a lógica de Main para EmployeeService

--- src/main/java/br/com/projedata/main/Main.java
+++ src/main/java/br/com/projedata/main/Main.java
@@ -1,6 +1,7 @@
 package br.com.projedata.main;
 
 import br.com.projedata.model.Employee;
+import br.com.projedata.service.EmployeeService;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,20 +11,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Employee> employees = new  ArrayList<Employee>();
+        EmployeeService employeeService = new EmployeeService();
 
-        employees.add(new Employee("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"));
-        employees.add(new Employee("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"));
-        employees.add(new Employee("Caio", LocalDate.of(1961, 5, 2), new BigDecimal("9836.14"), "Coordenador"));
-        employees.add(new Employee("Miguel", LocalDate.of(1988, 10, 14), new BigDecimal("19119.88"), "Diretor"));
-        employees.add(new Employee("Alice", LocalDate.of(1995, 1, 5), new BigDecimal("2234.68"), "Recepcionista"));
-        employees.add(new Employee("Heitor", LocalDate.of(1999, 11, 19), new BigDecimal("1582.72"), "Operador"));
-        employees.add(new Employee("Arthur", LocalDate.of(1993, 3, 31), new BigDecimal("4071.84"), "Contador"));
-        employees.add(new Employee("Laura", LocalDate.of(1994, 7, 8), new BigDecimal("3017.45"), "Gerente"));
-        employees.add(new Employee("Heloísa", LocalDate.of(2003, 5, 24), new BigDecimal("1606.85"), "Eletricista"));
-        employees.add(new Employee("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente"));
+        List<Employee> employees = employeeService.insertEmployees();
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
 
-        employees.forEach(System.out::println);
 
     }
 }
